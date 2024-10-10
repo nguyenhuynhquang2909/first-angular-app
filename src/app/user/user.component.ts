ef6c2661fbe2acfab4cacb74e53dd7e541cbe412
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-10-10 17:10:35 +0700

    Update task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-user';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,14 +8,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]) ;
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string; 
+  @Input({ required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+
+  
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
